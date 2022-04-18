8b53020c0bb66cd5409e145cc2885e8560db24a4
imcvk <chaitanyakulkarni789@gmail.com>
2022-04-18 22:30:13 +0530

    RequestDispatcher

---
+++ RequestDispatcher-demo/src/main/java/com/jspservlet/sampleform/Servlet2.java
@@ -0,0 +1,24 @@
+package com.jspservlet.sampleform;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Servlet2", value = "/Servlet2")
+public class Servlet2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+        out.println(
+                "<h2>Successfully registered</>"
+        );
+    }
+}
